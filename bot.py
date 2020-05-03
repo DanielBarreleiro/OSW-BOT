15a6075df56cd11f271578cb0534231e83749a5b
Daniel Barreleiro <danielalvesbarreleiro@gmail.com>
2020-05-03 22:20:48 +0100

    New commands: !all / !pstats + name / !stats + name
    Made 3 new commands:
    !all - A dangerous command that will take very long to complete, will give all members alliance data.
    !pstats - will give a player data | Usage: !pstats [name] (without [ ])
    !stats - will give player data inside the alliance | Usage: !stats [name] (without [ ])

--- bot.py
+++ bot.py
@@ -8,10 +8,12 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('Logged in as {0.user}'.format(client))
+    await client.change_presence(status=discord.Status.online, activity = discord.Activity(type=discord.ActivityType.watching, name="the Alliance"))
 
 @client.event
 async def on_message(message):
+    airname = ''
     if message.author == client.user:
         return
 
@@ -23,12 +25,48 @@ async def on_message(message):
         now = now.strftime("%H:%M")
         await message.channel.send('Alliance Value at ' + str(now) + ': $' + str(results['alliance'][0]['value']))
 
+    #TOO DANGEROUS TO BE USED
+    #elif message.content.startswith('!all'):
+    #    response = requests.post(url)
+    #    response.raise_for_status()
+    #    results = response.json()
+    #    now = datetime.now()
+    #    now = now.strftime("%H:%M")
+    #    n = 0
+    #    for member in results['members']:
+    #        await message.channel.send('Alliance Members Stats at ' + str(now))
+    #        await message.channel.send('Member: ' + str(results['members'][n]['company']))
+    #        await message.channel.send('Flights: ' + str(results['members'][n]['flights']))
+    #        await message.channel.send('Contributed: ' + str(results['members'][n]['contributed']))
+    #        await message.channel.send('IPO Value: ' + str(results['members'][n]['shareValue']))
+    #        await message.channel.send('-----------------------------')
+    #        n = n + 1
+
+    elif message.content.startswith('!pstats' + airname):
+        airname = message.content
+        airname = airname.replace('!pstats ', '')
+        response = requests.post(user_url + '&user=' + airname)
+        response.raise_for_status()
+        results = response.json()
+        print(user_url + '&user=' + airname)
+        await message.channel.send('Showing results for Airline: ' + airname)
+        await message.channel.send('Level: ' + str(results['user']['level']))
+        await message.channel.send('Share Value: $' + str(results['user']['share']))
+        await message.channel.send('Fleet: ' + str(results['user']['fleet']))
+        await message.channel.send('Routes: ' + str(results['user']['routes']))
+
+    elif message.content.startswith('!stats' + airname):
+        airname = message.content
+        airname = airname.replace('!stats ', '')
+        response = requests.post(url)
+        response.raise_for_status()
+        results = response.json()
+        for member in results['members']:
+            if member['company'] == airname:
+                await message.channel.send('Showing results for Airline: ' + str(airname))
+                await message.channel.send('Flights: ' + str(member['flights']))
+                cont = member['contributed'] / 1000
+                await message.channel.send('Contribution: $' + str(cont))
+                await message.channel.send('Share Value: $' + str(member['shareValue']))
+
 client.run(token)
-
-
-#    for member in data['members']:
-#        print('Member: ' + str(member['company']))
-#        print('Flights: ' + str(member['flights']))
-#        print('Contribution: ' + str(member['contributed']))
-#        print('Share Value: ' + str(member['shareValue']))
-#        print('-------------------')
