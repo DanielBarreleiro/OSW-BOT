9c6116c0368ae674a9e4384c3d016e42f2902230
Daniel Barreleiro <danielalvesbarreleiro@gmail.com>
2020-05-06 23:36:29 +0100

    Now using the webhook message design, command to edit gsheet
    Messages now use webhook-like design.
    New commands to insert alliance value into gsheets.

--- bot.py
+++ bot.py
@@ -14,18 +14,38 @@ gc = gspread.authorize(creds)
 
 client = discord.Client()
 
+sheet = gc.open("AllianceValue")
 #GET CURRENT MONTH
 month_n = datetime.today().strftime('%m')
 month = calendar.month_name[int(month_n)]
-#GET SHEET AND WORKSHEET BASED ON MONTH
-sheet = gc.open("AllianceValue")
+#GET WORKSHEET BASED ON MONTH
 worksheet = sheet.worksheet(month)
-#worksheet.update_cell(10, 3, 'test')
+
+#GET CURRENT DAY + 1
+day = int(datetime.today().strftime('%d')) + 1
+#GET ROW BY DAY
+midday_col = 2
+midnight_col = 3
+#worksheet.update_cell(10, midday_row, 'test')
 #------------------
-now = datetime.now().strftime('%H:%M')
-#---
-f = open("12row.txt","w+")
-#---
+#now = datetime.now().strftime('%H')
+#-------
+#loop(seconds=2, minutes=0, hours=0, count=None, reconnect=True)
+#if now == '12':
+#if message.content.startswith('!dayval'):
+#    response = requests.post(url)
+#    response.raise_for_status()
+#    results = response.json()
+#    worksheet.update_cell(day, midday_col, str(results['alliance'][0]['value']))
+#if now == '21':
+#    print('k')
+#if now == '00':
+#if message.content.startswith('!nightval'):
+#    response = requests.post(url)
+#    response.raise_for_status()
+#    results = response.json()
+#    worksheet.update_cell(day, midnight_col, str(results['alliance'][0]['value']))
+
 
 @client.event
 async def on_ready():
@@ -39,26 +59,27 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    #if now = '12:00':
-    #    response = requests.post(url)
-    #    response.raise_for_status()
-    #    results = response.json()
-    #    #worksheet.update_cell(7, 2, str(results['alliance'][0]['value']))
-    #    f.write("%d" % (i + 1))
+    if message.content.startswith('!dayval'):
+        response = requests.post(url)
+        response.raise_for_status()
+        results = response.json()
+        worksheet.update_cell(day, midday_col, str(results['alliance'][0]['value']))
 
-    #if now = '23:00':
-    #    response = requests.post(url)
-    #    response.raise_for_status()
-    #    results = response.json()
-        #worksheet.update_cell(7, 2, str(results['alliance'][0]['value']))
-   #     f.write("%d" % (i + 1))
+    if message.content.startswith('!nightval'):
+        response = requests.post(url)
+        response.raise_for_status()
+        results = response.json()
+        worksheet.update_cell(day, midnight_col, str(results['alliance'][0]['value']))
 
     if message.content.startswith('!alliance'):
         response = requests.post(url)
         response.raise_for_status()
         results = response.json()
-        await message.channel.send('Alliance Value: $' + str(results['alliance'][0]['value']) + "\n" +
-        'Rank: ' + str(results['alliance'][0]['rank']))
+        embed = discord.Embed(title="Alliance Stats", color=0x00ffff)
+        embed.add_field(name="Value", value='$' + str(results['alliance'][0]['value']), inline=True)
+        embed.add_field(name="Rank", value=str(results['alliance'][0]['rank']), inline=True)
+        embed.set_footer(text="By: TOYOTA - AIR")
+        await message.channel.send(embed=embed)
 
     elif message.content.startswith('!pstats' + airname):
         airname = message.content
@@ -67,11 +88,13 @@ async def on_message(message):
             response = requests.post(user_url + '&user=' + airname)
             response.raise_for_status()
             results = response.json()
-            await message.channel.send('Airline: ' + airname + "\n" +
-            'Level: ' + str(results['user']['level']) + "\n" +
-            'Share Value: $' + str(results['user']['share']) + "\n" +
-            'Fleet: ' + str(results['user']['fleet']) + "\n" +
-            'Routes: ' + str(results['user']['routes']))
+            embed = discord.Embed(title= airname + "  |  Stats", color=0x00ffff)
+            embed.add_field(name="Level", value=str(results['user']['level']), inline=False)
+            embed.add_field(name="Share Value", value='$' + str(results['user']['share']), inline=False)
+            embed.add_field(name="Fleet", value=str(results['user']['fleet']), inline=False)
+            embed.add_field(name="Routes", value=str(results['user']['routes']), inline=False)
+            embed.set_footer(text="By: TOYOTA - AIR")
+            await message.channel.send(embed=embed)
         except:
             await message.channel.send("Airline not found!")
 
@@ -85,10 +108,13 @@ async def on_message(message):
             try:
                 if member['company'] == airname:
                     cont = member['contributed'] / 1000
-                    await message.channel.send('Airline: ' + str(airname) + "\n" +
-                    'Flights: ' + str(member['flights']) + "\n" +
-                    'Contribution: $' + str(cont) + "\n" +
-                    'Share Value: $' + str(member['shareValue']))
+                    airname = airname.replace('!stats ', '')
+                    embed = discord.Embed(title=airname + "  |  Alliance Stats", color=0x00ffff)
+                    embed.add_field(name="Flights", value=str(member['flights']), inline=False)
+                    embed.add_field(name="Contribution", value='$' + str(cont), inline=False)
+                    embed.add_field(name="Share Value", value='$' + str(member['shareValue']), inline=False)
+                    embed.set_footer(text="By: TOYOTA - AIR")
+                    await message.channel.send(embed=embed)
             except:
                 await message.channel.send("Airline not found!")
 
@@ -104,8 +130,9 @@ async def on_message(message):
         await channel.send('CO2 <@&706951098132594708> at $' + str(price) + ' !')
 
     #elif message.content.startswith('!osw'):
-    #    channel = client.get_channel(706963574903275642)
-    #    await channel.send("Don't worry, the bot hasn't freaked out! Its just being updated!" + "\n" +
-    #    'Tag <@&706951098132594708>  !')
+     #   channel = client.get_channel(703342736568352809)
+        #await channel.send("Don't worry, the bot hasn't freaked out! Its just being updated!")
+        #+ "\n" + 'Tag <@&703342736568352809>  !')
+      #  await channel.send(embed=embed)
 
 client.run(token)
